6e04ecabc21965dca227b5c16ec97446f28ccf2c
ddalam <dychoi.dev@gmail.com>
2022-10-19 22:58:36 +0900

    상태 없는 서블릿 - 상태 없는 객체는 항상 스레드 안전하다

---
+++ src/main/java/com/study/concurrency/threadsafety/StatelessFactorizer.java
@@ -0,0 +1,28 @@
+package com.study.concurrency.threadsafety;
+
+
+import com.study.concurrency.annotation.ThreadSafe;
+import java.math.BigInteger;
+import javax.servlet.*;
+
+@ThreadSafe
+public class StatelessFactorizer extends GenericServlet implements Servlet {
+
+	public void service(ServletRequest req, ServletResponse resp) {
+		BigInteger i = extractFromRequest(req);
+		BigInteger[] factors = factor(i);
+		encodeIntoResponse(resp, factors);
+	}
+
+	void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+	}
+
+	BigInteger extractFromRequest(ServletRequest req) {
+		return new BigInteger("7");
+	}
+
+	BigInteger[] factor(BigInteger i) {
+		// Doesn't really factor
+		return new BigInteger[] { i };
+	}
+}
