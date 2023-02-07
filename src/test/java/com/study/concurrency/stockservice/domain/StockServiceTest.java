0776d71ef6e4264f7c33f4280316e2cf8ee34bd1
ddalam <dychoi.dev@gmail.com>
2023-02-07 19:39:59 +0900

    feat(stock/decrease): Race Condition 발생으로 재고 감소가 실패할수도 있는 테스트 추가

--- src/test/java/com/study/concurrency/stockservice/domain/StockServiceTest.java
+++ src/test/java/com/study/concurrency/stockservice/domain/StockServiceTest.java
@@ -3,6 +3,9 @@ package com.study.concurrency.stockservice.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import com.study.concurrency.stockservice.infrastructure.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,4 +39,28 @@ class StockServiceTest {
 		assertThat(stock.getQuantity()).isEqualTo(99L);
 	}
 
+	@Test
+	void decreaseStockRequest100Times() throws InterruptedException {
+	  // given
+	  int threadCount = 100;
+		final ExecutorService executorService = Executors.newFixedThreadPool(32);
+		final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+		// when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					countDownLatch.countDown();
+				}
+			});
+		}
+
+		countDownLatch.await();
+	  
+	  // then
+		final Stock stock = stockRepository.findById(1L).orElseThrow();
+		assertThat(stock.getQuantity()).isEqualTo(0L);
+	}
 }
